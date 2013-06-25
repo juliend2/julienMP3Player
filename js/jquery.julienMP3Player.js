c562b1eea1fb7ea9952778af78ccca01c9ace464
Julien Desrosiers <juliend2@gmail.com>
2013-06-25 10:00:37 -0400

    fix bug in IE<=9 where indexOf is not supported

--- js/jquery.julienMP3Player.js
+++ js/jquery.julienMP3Player.js
@@ -1,5 +1,40 @@
 (function($){
 
+  // IE <9 does not support indexOf (see: http://stackoverflow.com/a/10923456/242404 )
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+      "use strict";
+      if (this == null) {
+        throw new TypeError();
+      }
+      var t = Object(this);
+      var len = t.length >>> 0;
+
+      if (len === 0) {
+        return -1;
+      }
+      var n = 0;
+      if (arguments.length > 1) {
+        n = Number(arguments[1]);
+        if (n != n) { // shortcut for verifying if it's NaN
+          n = 0;
+        } else if (n != 0 && n != Infinity && n != -Infinity) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+        }
+      }
+      if (n >= len) {
+        return -1;
+      }
+      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+      for (; k < len; k++) {
+        if (k in t && t[k] === searchElement) {
+          return k;
+        }
+      }
+      return -1;
+    }
+  }
+
   $.julienMP3Player = { version: '0.1.5' };
 
   var settings = {
